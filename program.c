bbf6c526c3caf9eb1800223cb4c061cc933b5648
jchen9182 <jchen9182@gmail.com>
2020-02-02 14:42:59 -0500

    finished

--- program.c
+++ program.c
@@ -7,10 +7,25 @@ int main() {
 
     fprintf(fp, "P3\n500 500\n255");
 
+    int counter = 1;
     for (int i = 0; i < 500; i++) {
         fprintf(fp, "\n");
-        for (int j = 0; j < 500; j++) {
-            fprintf(fp, "255 255 0 ");
+        
+        if (i % 51 == 0 && i != 0) counter++;
+
+        if (! (i < 51 * counter - 10)) {
+            for (int j = 0; j < 500; j++) {
+                fprintf(fp, "200 200 200 ");
+            }
+        }
+        else {
+            int iters = 1;
+            for (int j = 0; j < 500; j++) {
+                if (j % 51 == 0 && j != 0) iters++;
+
+                if (j < 51 * iters - 10) fprintf(fp, "0 0 0 ");
+                else fprintf(fp, "200 200 200 ");
+            }
         }
     }
 
